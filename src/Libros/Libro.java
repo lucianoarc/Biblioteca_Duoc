e769e24891b2debf0ffdec7d2c9ed312ccc93ec6
lucia <lucia@Galaxy-Book-3>
2024-06-10 18:49:51 -0400

    Libros clase java carga inicial

---
+++ src/Libros/Libro.java
@@ -0,0 +1,35 @@
+
+package Libros;
+
+
+public class Libro {
+    private String titulo;
+    private String autor;
+    private boolean disponible;
+
+    public Libro(String titulo, String autor) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.disponible = true;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public boolean estaDisponible() {
+        return disponible;
+    }
+
+    public void prestar() {
+        disponible = false;
+    }
+
+    public void devolver() {
+        disponible = true;
+    }
+}
